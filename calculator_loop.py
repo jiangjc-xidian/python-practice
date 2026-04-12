9ce8cc63cc3bd7ca68d723cef4ad34e13f64bd0b
jiangjc-xidian <2041883620@qq.com>
2026-04-12 20:16:03 +0800

    完善了一些功能

--- calculator_loop.py
+++ calculator_loop.py
@@ -4,19 +4,35 @@ count = 0   # 记录次数
 
 while True:
     # 获取第一个数字，检查是否要退出
-    first = input("请输入第一个数字（或输入quit退出):")
+    first = input("请输入第一个数字（或输入quit退出,输入restart重新开始):")
     if first == "quit":
         print("你共计算了",count,"次")   # 退出时显示次数
         print("再见!")
         break
+    elif first == "restart":
+        print("重新开始本次计算")
+        continue
 
-    # 获取运算符和第二个数字
+    # 获取运算符
     op = input("请输入运算符（+，-，*，/，%）:")
+    if op == "restart":
+        print("重新开始本次计算")
+        continue
+    # 获取第二个数字
     second = input("请输入第二个数字:")
+    if second == "restart":
+        print("重新开始本次计算")
+        continue
 
-    # 转化成数字（注意：此时已经排除了 quit）
-    num1 = float(first)
-    num2 = float(second)
+    #所有输入都正常，转化成数字（注意：此时已经排除了 quit）
+    try:
+        
+        num1 = float(first)
+        num2 = float(second)
+    except ValueError:
+        print("请输入有效的数字!")
+        continue
+    
 
     # 计算
     if op == "+":
